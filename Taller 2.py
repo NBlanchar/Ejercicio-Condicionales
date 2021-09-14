4b26562008053149012494cb03d8b92dbcbf9968
Nefer Blanchar <nblancha1@cuc.edu.co>
2021-09-13 19:22:05 -0500

    Ejercicio 4

--- Taller 2.py
+++ Taller 2.py
@@ -16,10 +16,9 @@ if(num_camisas > 0):
     print(f'El total sin descuento es: ${acumulador:,}')
     if(num_camisas >= 3):
         total = acumulador - (acumulador * 0.3)
-        print(f'El total a pagar es: ${total:,}')
     else:
         total = acumulador - (acumulador * 0.1)
-        print(f'El total a pagar es: ${total:,}')
+    print(f'El total a pagar es: ${total:,}')
 else:
     print('Por lo menos debe comprar una camiseta')
 
@@ -30,15 +29,14 @@ print(f'el numero al azar es: {num}')
 if(total_compra > 0):
     if(num >= 74):
         total = total_compra - (total_compra * 0.2)
-        print(f'El total a pagar es: ${total:,}')
     else:
         total = total_compra - (total_compra * 0.15)
-        print(f'El total a pagar es: ${total:,}')
+    print(f'El total a pagar es: ${total:,}')
 else:
     print("Digite un valor a pagar valido")
 
 # Ejercico 3
-fianza = float(input('Digite el valor de la fianza: $'))
+fianza = float(input('Digite el valor de la fianza: '))
 if(fianza > 0):
     if(fianza < 50000):
         cuota = fianza * 0.03
@@ -49,6 +47,26 @@ else:
     print("Digite un valor a pagar valido")
 
 # Ejercicio 4
+produccion = int(input('Digite el total de dias de producción: '))
+if (produccion >= 14):
+    puntos = int(input('Digite la cantidad de puntos de control ambiental: '))
+    ganancias = float(input('Digite el valor de las ganancias diarias: '))
+    acumulador = 0
+    for x in range(puntos):
+        D_uno = int(input(f'Emision del punto {x+1} el primer dia: '))
+        D_dos = int(input(f'Emision del punto {x+1} el segundo dia: '))
+        D_tres = int(input(f'Emision del punto {x+1} el tercer dia: '))
+        D_cuatro = int(input(f'Emision del punto {x+1} el cuarto dia: '))
+        D_cinco = int(input(f'Emision del punto {x+1} el quinto dia: '))
+        acumulador = acumulador + D_cinco + D_cuatro + D_tres + D_dos + D_uno
+    promedio = acumulador / (puntos * 5)
+    if(promedio > 170):
+        perdida = (7 * ganancias) + ((produccion - 7) * ganancias * 0.5)
+    else:
+        perdida = 0
+    print(f'las perdidas generadas por la inspección es: ${perdida:,}')
+else:
+    print('La producción debe duarar minimo 14 dias')
 
 # Ejercicio 5
 devaluacion = float(input('Porcentaje de devalucación del vehiculo: '))
@@ -82,20 +100,20 @@ if(costo > 0):
     elif (marca == 'NOSY'):
         costo = costo - (costo * 0.05)
     elif(costo >= 2000):
-        costo = costo - (costo * 0.05)
+        costo = costo - (costo * 0.1)
     costo = costo + (costo * 0.16)
     print(f'EL total a pagar por el equipo es: ${costo:,}')
 else:
     print("Digite valor valido")
 
 # Ejercicio 8
-precio = float(input('Digite el moto de la compra: '))
+precio = float(input('Digite el monto de la compra: '))
 prestamo = 0
 if(precio > 0):
     if(precio > 500000):
         inversion = precio * 0.55
         prestamo = precio * 0.30
-        credito = precio * 0.15        
+        credito = precio * 0.15
     else:
         inversion = precio * 0.7
         credito = precio * 0.3
